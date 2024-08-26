af7024de675c8c06d1f585116ff10815452d416a
ranyoung <fks1311@gmail.com>
2024-08-26 23:31:42 +0900

    redux 사용하기

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
+number.innerText = 0;
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// reducer 함수
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  // Switch ~ case 구문으로 변경
+  if (action.type === ADD) {
+    return count + 1;
+  } else if (action.type === MINUS) {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
-const handleMinus = () => {
-  count--;
-  updateText();
+// store 생성
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// store의 state의 변화를 감지(구독)
+countStore.subscribe(onChange);
+
+// dispatch => store의 action 호출
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
